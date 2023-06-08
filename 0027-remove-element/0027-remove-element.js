faef8ad74fff3285664c74231e4149e29ce8d654
Andrés Camilo Vélez <randombluemail@gmail.com>
2023-06-08 09:55:17 -0500

    Time: 67 ms (18.36%), Space: 41.9 MB (69.85%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0;
+    for(let i = 0 ; i < nums.length ; i++){
+        if(nums[i] !== val){
+            k += 1;
+        } else {
+           nums[i] = "_"
+        }
+    }
+    nums.sort()
+    return k;
+};
